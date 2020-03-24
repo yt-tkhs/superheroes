1cd3dd2562e8b40fededa24a74fb32f66ce02a7e
Yuta Takahashi <yt.fp.0130@gmail.com>
2020-03-24 23:11:16 +0900

    Init project

---
+++ app/src/main/java/app/ytak/superheroes/MainActivity.kt
@@ -0,0 +1,11 @@
+package app.ytak.superheroes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
